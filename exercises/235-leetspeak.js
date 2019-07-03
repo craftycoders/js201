f40bd97ddd817a74a674dd77dfe3b85ff7bfe158
Pia Soy <piasoy4@gmail.com>
2019-07-03 18:58:42 -0500

    solve 235-leetspeak

--- exercises/235-leetspeak.js
+++ exercises/235-leetspeak.js
@@ -14,3 +14,30 @@
 // Examples:
 // leetspeak('Leet') --> "l337"
 // leetspeak('ORANGE') --> "0r4n63"
+let conversionList = {
+    A: 4,
+    E: 3,
+    G: 6,
+    I: 1,
+    O: 0,
+    S: 5,
+    T: 7
+}
+
+function leetspeak(str){
+    //create array of keys
+    let convListArr = Object.keys(conversionList);         
+    let convStr = '';
+
+    //loop through  string
+    for(let idx = 0; idx < str.length; idx++){             
+        let upperChar = str[idx].toUpperCase()
+       
+        //if character is NOT a key, concatenate original character to new string
+       if((convListArr.indexOf(upperChar)) === -1){convStr += str[idx]}
+    
+       //otherwise character IS a key, concatenate the key:value to new string
+       else {convStr += conversionList[upperChar]}
+    }
+    return convStr.toLowerCase();
+}
